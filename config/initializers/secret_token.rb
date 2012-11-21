51c924b507f0685170194aa53a9c935b81f81bfb
Robert Fletcher <lobatifricha@gmail.com>
2012-11-20 23:30:27 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Academy::Application.config.secret_token = 'f89a27b51ce8f79adc3c08c691007cabf55691a5c326b32ac0eb24536e0163a9ec5e4e0c99505217db4ec50f29a64795f472116c620bacebe83be5253ccc09c3'
